9be51511d29c5a660bd7cd25993cdae6ec8922d6
Chasitty Ayala <70674480+chasittya@users.noreply.github.com>
2024-04-28 04:06:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from flask import Flask,render_template
 app = Flask(__name__)
 app.secret_key = 'SuperSecretKey'
 
-
+#login page
 @app.route('/login')
 def home():
-    return render_template('login.html') # pass data to our template
+    return render_template('login.html') 
 
+#register page
 @app.route('/register')
 def registration():
     return render_template('registration.html')
 
+#dashboard page
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
+#allows to refresh without closing out virtual environment
 if __name__ == '__main__':
     app.run(debug=True)
 
