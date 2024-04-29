b7ffd441e0cc6550860e2f3ac9470288fc9eeea4
Chasitty Ayala <70674480+chasittya@users.noreply.github.com>
2024-04-29 15:51:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,9 @@ def contactUs():
 def history():
     return render_template('history.html')
 
-@app.route('/profile',methods=['GET'])
-def profile():
-    return render_template('profile.html')
 
 #get and post requests
+#login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -49,9 +47,47 @@ def login():
         else:
             flash("Invalid email or password!")
             return redirect('/login')
+        
+#profile
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'user_id' not in session:
+        flash('Must be logged in to access profile')
+        return redirect('/login')
 
+    if request.method == 'GET':
+        # Get patient first name and last name if available
+        mysql = connectToMySQL('clinic_manage')
+        query = "SELECT FName, LName FROM patients WHERE userID = %(user_id)s"
+        data = {'user_id': session['user_id']}
+        patient_info = mysql.query_db(query, data)
+
+        # Render profile template with patient info
+        return render_template('profile.html', patient_info=patient_info[0] if patient_info else None)
+
+    elif request.method == 'POST':
+        # Update patient info
+        gender = request.form['gender']
+        birthday = request.form['birthday']
+        contactPhone = request.form['contactPhone']
+        medicalHistory = request.form['medicalHistory']
+        notes = request.form['notes']
+
+        mysql = connectToMySQL('clinic_manage')
+        query = "UPDATE patients SET gender=%(gender)s, birthday = %(birthday)s, contactPhone = %(contactPhone)s, medicalHistory = %(medicalHistory)s, notes = %(notes)s WHERE userID = %(user_id)s"
+        data = {
+            'gender': gender,
+            'birthday': birthday,
+            'contactPhone': contactPhone,
+            'medicalHistory': medicalHistory,
+            'notes': notes,
+            'user_id': session['user_id']
+        }
+        mysql.query_db(query, data)
+
+        flash('Profile updated successfully!')
+        return redirect('/dashboard')
     
-#registration post and get route
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'GET':
@@ -59,53 +95,81 @@ def register():
     elif request.method == 'POST':
         is_valid = True
     
-    #Registration form validation 
-    if len(request.form['form_first_name']) < 2:
-        flash('First Name is required')
-        is_valid = False
-    
-    if len(request.form['form_last_name']) < 2:
-        flash('Last Name is required')
-        is_valid = False
+        # Registration form validation 
+        role = request.form['form_role']
 
-    if not EMAIL_REGEX.match(request.form['form_email']):
-        flash("Invalid email address!")
-        is_valid = False
-    else:
-        mysql = connectToMySQL('clinic_manage')
-        query = "SELECT * FROM users WHERE email = %(form_email)s"
-        data={'form_email': request.form['form_email']}
-        result= mysql.query_db(query,data)
-        if result:
-            flash('Email Already exists')
+        if role not in ['patient', 'staff', 'admin']:
+            flash("Invalid role")
+            return redirect('/register')
+        
+        if len(request.form['form_first_name']) < 2:
+            flash('First Name is required')
             is_valid = False
         
-    if len(request.form['form_password']) < 8:
-        flash('Password must be 8 characters long')
-        is_valid = False
+        if len(request.form['form_last_name']) < 2:
+            flash('Last Name is required')
+            is_valid = False
 
-    if request.form['form_password'] != request.form['form_confirm_password']:
-        flash("Passwords must match!")
-        is_valid = False
+        if not EMAIL_REGEX.match(request.form['form_email']):
+            flash("Invalid email address!")
+            is_valid = False
+        else:
+            mysql = connectToMySQL('clinic_manage')
+            query = "SELECT * FROM users WHERE email = %(form_email)s"
+            data={'form_email': request.form['form_email']}
+            result= mysql.query_db(query,data)
+            if result:
+                flash('Email Already exists')
+                is_valid = False
+            
+        if len(request.form['form_password']) < 8:
+            flash('Password must be 8 characters long')
+            is_valid = False
 
-    if not is_valid:
-        return redirect('/register')
-    else:
-        pw_hash = bcrypt.generate_password_hash(request.form['form_password'])
-        mysql = connectToMySQL('clinic_manage')
-        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(f_name)s, %(l_name)s, %(email)s, %(hashed_pw)s);"
-        data={
-            'f_name': request.form['form_first_name'],
-            'l_name': request.form['form_last_name'],
-            'email': request.form['form_email'],
-            'hashed_pw': pw_hash
-        }
-        new_user_id = mysql.query_db(query,data)
+        if request.form['form_password'] != request.form['form_confirm_password']:
+            flash("Passwords must match!")
+            is_valid = False
 
+        if not is_valid:
+            return redirect('/register')
+        else:
+            pw_hash = bcrypt.generate_password_hash(request.form['form_password'])
+            mysql = connectToMySQL('clinic_manage')
+            query = "INSERT INTO users (first_name, last_name, email, password, role) VALUES (%(f_name)s, %(l_name)s, %(email)s, %(hashed_pw)s, %(role)s);"
+            data={
+                'f_name': request.form['form_first_name'],
+                'l_name': request.form['form_last_name'],
+                'email': request.form['form_email'],
+                'hashed_pw': pw_hash,
+                'role': role
+            }
 
-        session['user_id']= new_user_id
-        flash('Registration successful')
-        return redirect('/dashboard')
+            # Based on the user's role, insert their information into the respective table
+            new_user_id = mysql.query_db(query, data)
+
+            if new_user_id: 
+                new_user_id = new_user_id  
+
+                if role == 'patient':
+                    query = "INSERT INTO patients (userID, FName, LName) VALUES (%(user_id)s, %(f_name)s, %(l_name)s);"
+                elif role == 'staff':
+                    query = "INSERT INTO staff (userID, FName, LName) VALUES (%(user_id)s, %(f_name)s, %(l_name)s);"
+                elif role == 'admin':
+                    query = "INSERT INTO admin (userID, FName, LName) VALUES (%(user_id)s, %(f_name)s, %(l_name)s);"
+                    session['user_id'] = new_user_id
+                    flash('Registration successful')
+                
+                mysql.query_db(query, {'user_id': new_user_id, 'f_name': request.form['form_first_name'], 'l_name': request.form['form_last_name']})
+                
+                if role == 'patient':
+                    return redirect('/dashboard')
+                elif role == 'staff':
+                    return redirect('/staff_dashboard')
+                elif role == 'admin':
+                    return redirect('/admin_dashboard')
+            else:
+                flash("Failed to register user")
+                return redirect('/register')
     
 #dashboard route
 @app.route('/dashboard')
