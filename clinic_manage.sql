6d4ec25e4d4c5978ea076c473b651feb1acb1af6
Chasitty Ayala <70674480+chasittya@users.noreply.github.com>
2024-04-29 20:17:02 -0500

    Update clinic_manage.sql

--- clinic_manage.sql
+++ clinic_manage.sql
@@ -1,21 +1,14 @@
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
-CREATE SCHEMA IF NOT EXISTS `clinic_manage` DEFAULT CHARACTER SET utf8 ;
-USE `clinic_manage` ;
-
 -- Table `users` for authentication
 CREATE TABLE IF NOT EXISTS `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `FName` VARCHAR(255),
+  `LName` VARCHAR(255),
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `role` VARCHAR(10) NOT NULL DEFAULT 'patient', 
+  `role` ENUM('patient', 'staff', 'admin') NOT NULL DEFAULT 'patient', 
   `created_at` DATETIME NULL DEFAULT NOW(),
   `updated_at` DATETIME NULL DEFAULT NOW() ON UPDATE NOW()
 ) ENGINE = InnoDB;
-
 
 -- Table `patients`
 CREATE TABLE IF NOT EXISTS `patients` (
@@ -23,7 +16,6 @@ CREATE TABLE IF NOT EXISTS `patients` (
   `userID` INT,
   `FName` VARCHAR(255),
   `LName` VARCHAR(255),
-  `role`  VARCHAR(50),
   `SSN` VARCHAR(255),
   `birthday` DATE,
   `gender` CHAR(1),
@@ -33,11 +25,9 @@ CREATE TABLE IF NOT EXISTS `patients` (
   FOREIGN KEY (`userID`) REFERENCES `users`(`id`)
 ) ENGINE=InnoDB;
 
-
-
 -- Table `staff`
 CREATE TABLE IF NOT EXISTS `staff` (
-  `staffID` INT AUTO_INCREMENT PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `userID` INT,
   `FName` VARCHAR(255),
   `LName` VARCHAR(255),
@@ -47,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `staff` (
 
 -- Table `admin`
 CREATE TABLE IF NOT EXISTS `admin` (
-  `adminID` INT AUTO_INCREMENT PRIMARY KEY,
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
   `userID` INT,
   `FName` VARCHAR(255),
   `LName` VARCHAR(255),
@@ -61,11 +51,8 @@ CREATE TABLE IF NOT EXISTS `appointments` (
   `patientID` INT,
   `staffID` INT,
   `appointment_datetime` DATETIME,
-  FOREIGN KEY (`staffID`) REFERENCES `staff` (`patientID`),
+  FOREIGN KEY (`staffID`) REFERENCES `staff` (`id`),
   FOREIGN KEY (`patientID`) REFERENCES `patients`(`patientID`)
 ) ENGINE=InnoDB;
 
--- Reset SQL mode and foreign key checks
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
